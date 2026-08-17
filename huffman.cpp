8c7d81625b35e0387f8c753891d759fa8eb29b73
Anvi Wadhwa <anvi.wadhwa2024@vitstudent.ac.in>
2026-08-17 23:40:11 +0530

    Add initial structure for Huffman coding

---
+++ huffman.cpp
@@ -0,0 +1,15 @@
+// #include<iostream>
+// #include<algorithm>
+// #include<queue>
+// using namespace std;
+// struct Node{
+//   char ch;
+//   int freq;
+//   int l;
+//   int r;
+// };
+// struct compare{
+//   bool operator()(int a, int b){
+//     return a>b; 
+//   }
+// }
